e5539388da6231cbf360903d3de2e57c08614e0e
Abhinandan Thakur <73791479+abhinandant34@users.noreply.github.com>
2022-02-07 16:31:10 +0530

    initial commit

--- jyc/src/App.js
+++ jyc/src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from './components/Header/Header';      // aise dalna hai file ka link, make sure krna ki folder achi tarah se hirearchy mai rahe
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header/>          {/* aise add krna hai components */}
+
     </div>
   );
 }
---
+++ jyc/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+import logo from './images/jyclogo.png'
+function Header() {
+  return<div className='header'>
+      <img className='header-logo' src={logo}/>
+      This is a sample 
+      aise hi sare components bnane hai 
+  </div>;
+}
+
+export default Header;
